7fc8665825c97d7cdae36775ecd31c5613486845
Hector Coello <hector_m3@live.com.mx>
2023-01-30 08:35:37 -0600

    Removed set header

--- core/View.php
+++ core/View.php
@@ -4,12 +4,6 @@ namespace Core;
 
 class View {
 
-    public function set_header($code, $content_type)
-    {
-        header('Content-Type: ' . $content_type);
-        http_response_code($code);
-    }
-
     public static function get($_name, $_data=null){
 
         global $tinyapp_vars;
