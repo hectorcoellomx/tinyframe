625eb3bdd8d478637ff552ea2ec6fa19b69bd6b8
Hector Coello <hector_m3@live.com.mx>
2023-02-10 23:11:42 -0600

    Removed rest api controller (Test controller)

--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,16 @@
 <?php
 
 require_once './app/controllers/HomeController.php';
-require_once './app/controllers/StudentApiController.php';
 
 use Core\Route;
 use App\Controllers\HomeController;
 
 Route::register("get", "/", [ HomeController::class, 'index' ], [ 'test' ] );
-Route::register("get", "/api/student", [ HomeController::class, 'index' ] );
 
 Route::register("get", "/login", [ HomeController::class, 'access' ] );
 Route::register("post", "/login", [ HomeController::class, 'login' ] );
 Route::register("get", "/dashboard", [ HomeController::class, 'dashboard' ], [ 'test' ] ); 
 
-Route::register("get", "/profile/{id}", [ HomeController::class, 'dashboard' ], [ 'test' ] ); 
 
 
 
