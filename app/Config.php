6c0729796baf20d119a4bc123ba1a85e2aa0bbb4
Hector Coello <hector_m3@live.com.mx>
2024-09-12 23:57:30 -0600

    Se agregó configuración de sesión y archivos al env

--- app/Config.php
+++ app/Config.php
@@ -57,8 +57,10 @@ class Config
             
             $this->status_project =                      $_ENV['STATUS_PROJECT'];
             $this->base_url =                            $_ENV['BASE_URL'];
-            $this->session_id =                          $_ENV['SESSION_ID'];
             $this->databases_driver =                    $_ENV['DATABASES_DRIVER'];
+            $this->load_session =                        ($_ENV['LOAD_SESSION'] === 'true' || $_ENV['LOAD_SESSION'] === 'TRUE');
+            $this->session_id =                          $_ENV['SESSION_ID'];
+            $this->load_upload_files =                   ($_ENV['LOAD_UPLOAD_FILES'] === 'true' || $_ENV['LOAD_UPLOAD_FILES'] === 'TRUE');
 
             $this->databases['mysql']['host'] =          $_ENV['DB_MYSQL_HOST'];
             $this->databases['mysql']['user'] =          $_ENV['DB_MYSQL_USER'];
