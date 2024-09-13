bea99cb749555753d5ba00122cfa415a7d06bc04
Hector Coello <hector_m3@live.com.mx>
2024-09-13 08:13:14 -0600

    Se agregó middleware de prueba

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@ require_once './app/controllers/HomeController.php';
 use Core\Route;
 use App\Controllers\HomeController;
 
-Route::register("get", "/", [ HomeController::class, 'index' ] );
+Route::register("get", "/", [ HomeController::class, 'index' ], ['test'] );
 
 Route::register("get", "/login", [ HomeController::class, 'access' ] );
 Route::register("post", "/login", [ HomeController::class, 'login' ] );
