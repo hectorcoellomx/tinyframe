b210738ec0eab4e68665fe7bfb466a3017a68079
Hector Coello <hector_m3@live.com.mx>
2024-09-15 06:48:06 -0600

    Correcciones en middleware

--- app/middlewares/Test.php
+++ app/middlewares/Test.php
@@ -4,8 +4,9 @@ namespace App\Middlewares;
 
 class Test {
 
-    public static function run()
-    {
+    public function run(){
+        // Here is the code of your middleware
+        // You can do redirects with 'redir()' or return a json or whatever 
         return true;
     }
 
