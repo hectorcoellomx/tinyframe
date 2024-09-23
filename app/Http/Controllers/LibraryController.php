4b579e67fa303fab8384e18da7cf330eafda6301
Fernando de Jesús Sánchez Arias <fernando.sanchez34@unach.mx>
2024-09-23 15:45:20 -0600

    correción

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -3,29 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\users;
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
+
+use function Laravel\Prompts\error;
 
 class LibraryController extends Controller
 {
     public function store(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'name' => 'required|max:200',
-            'type' => 'required|integer',
-            'last_access' => 'required|date',
-        ]);
-
-        $user = users::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'type' => $request->input('type'),
-            'last_access' => $request->input('last_access')
-        ]);
-
-        return response()->json([
-            'message' => 'Usuario creado exitosamente',
-            'user' => $user
-        ], 201);
+        try {
+            $validated = $request->validate([
+                'email' => 'required|email',
+                'name' => 'required|max:200',
+                'type' => 'required|integer',
+                'last_access' => 'required|date',
+            ]);
+    
+            $user = Users::create($validated);
+    
+            return response()->json([
+                "success" => true,
+                "data" => $user,
+                'message' => 'ok',
+            ], 201);
+    
+        } catch (\Exception $e) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                'message' => "ha ocurrido un error inesperado",
+                'error' => [
+                    'code' => "...",
+                    'details' => "Error de conexión bd"
+                ]
+            ], 500);
+        }
     }
 }
