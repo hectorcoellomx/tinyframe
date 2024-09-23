d4e1801641723ed0eb1fc49fe57012aab626532d
Fernando de Jesús Sánchez Arias <fernando.sanchez34@unach.mx>
2024-09-23 15:36:15 -0600

    guardar_usuario

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\users;
 use Illuminate\Http\Request;
 
 class LibraryController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'name' => 'required|max:200',
+            'type' => 'required|integer',
+            'last_access' => 'required|date',
+        ]);
+
+        $user = users::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'type' => $request->input('type'),
+            'last_access' => $request->input('last_access')
+        ]);
+
+        return response()->json([
+            'message' => 'Usuario creado exitosamente',
+            'user' => $user
+        ], 201);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\LibraryController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/api/v1/users/{id}',[LibraryController::class, 'store'])->name('user.store');
+Route::post('/api/v1/users',[LibraryController::class, 'store'])->name('user.store');
 Route::get('/api/v1/books',[LibraryController::class, ''])->name(''); // ?filter=category&value=5
 Route::get('/api/v1/categories/',[LibraryController::class, ''])->name('');
 Route::get('/api/v1/collections/',[LibraryController::class, ''])->name('');
