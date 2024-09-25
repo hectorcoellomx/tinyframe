77590b6b64721427e8f1a2ccc72e5f675bfaedeb
Fernando de Jesús Sánchez Arias <fernando.sanchez34@unach.mx>
2024-09-25 09:39:21 -0600

    segundo end point

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\bookController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,10 +8,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::post('/users', [UserController::class, 'store']);
-
-// Route::post('/api/v1/users',[LibraryController::class, 'store'])->name('user.store');
-// Route::get('/api/v1/books',[LibraryController::class, 'book'])->name('show.book'); // ?filter=category&value=5
+Route::post('/v1/users', [UserController::class, 'store']);
+Route::get('/v1/books',[bookController::class, 'book'])->name('show.book'); // ?filter=category&value=5
 // Route::get('/api/v1/categories/',[LibraryController::class, ''])->name('');
 // Route::get('/api/v1/collections/',[LibraryController::class, ''])->name('');
 // Route::post('/api/v1/progress/',[LibraryController::class, ''])->name('');
