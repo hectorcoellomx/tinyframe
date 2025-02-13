1a701decf26087b355ff1712230e6600eff06409
GeorginaAndrea <geomendezhdz@gmail.com>
2025-02-13 15:05:02 -0600

    Ruta actualizada

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',function(){
-    return view('welcome');
+    return view('layouts.home');
+});
+
+Route::get('/books',function(){
+    return view('layouts.books');
 });
 
