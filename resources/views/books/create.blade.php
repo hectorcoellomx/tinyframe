f98baa9e8db52e618bfa5418e1c1620250375e0b
GeorginaAndrea <geomendezhdz@gmail.com>
2025-02-17 15:11:08 -0600

    Books: Vista create, edit e index agregadas

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,12 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Vista: crear libros</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Crear Libro')
+
+@section('content')
+    <div class="container">
+        <h2>Crear Libro</h2>
+        <form method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Título:</label>
+                <input type="text" name="title" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Descripción:</label>
+                <textarea name="description" class="form-control" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="year">Año:</label>
+                <input type="number" name="year" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+    </div>
+@endsection
