d694f9245c6180a385d28445d10598bcb813eb4f
GeorginaAndrea <geomendezhdz@gmail.com>
2025-02-18 11:42:30 -0600

    Collections: Vista create, edit e index agregadas

---
+++ resources/views/collections/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('title', 'Listado de Colecciones')
+
+@section('content')
+    <div class="container">
+        <h2>Listado de Colecciones</h2>
+        <a href="/collections/create"  class="btn btn-primary mb-3">Agregar Coleccion</a>
+
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    {{-- <th>Título</th>
+                    <th>Descripción</th>
+                    <th>Año</th>
+                    <th>Acciones</th> --}}
+                </tr>
+            </thead>
+            <tbody>
+                {{-- @foreach($categories as $category)
+                    <tr>
+                        <td>{{ $book->id }}</td>
+                        <td>{{ $book->title }}</td>
+                        <td>{{ $book->description }}</td>
+                        <td>{{ $book->year }}</td>
+                        <td>
+                            <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning btn-sm">Editar</a>
+                            <form action="{{ route('books.destroy', $book->id) }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach --}}
+            </tbody>
+        </table>
+    </div>
+@endsection
