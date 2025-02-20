83fb7721cb153eb5029f5f8c29b31980093bd576
GeorginaAndrea <geomendezhdz@gmail.com>
2025-02-20 11:11:26 -0600

    Books: Vista index actualizada

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -16,11 +16,12 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th>ID</th>
+                    <th>Id</th>
                     <th>Título</th>
+                    <th>Portada</th>
                     <th>Descripción</th>
                     <th>Año</th>
-                    <th>Acciones</th>
+                    <th>Estatus</th>
                 </tr>
             </thead>
             <tbody>
@@ -28,16 +29,18 @@
                     <tr>
                         <td>{{ $book->id }}</td>
                         <td>{{ $book->title }}</td>
+                        <td>{{ $book->cover_photo }}</td>
                         <td>{{ $book->description }}</td>
                         <td>{{ $book->year }}</td>
-                        <td>
+                        <td>{{ $book->status }}</td>
+                        {{-- <td>
                             <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning btn-sm">Editar</a>
                             <form action="{{ route('books.destroy', $book->id) }}" method="POST" style="display: inline;">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
                             </form>
-                        </td>
+                        </td> --}}
                     </tr>
                 @endforeach
             </tbody>
