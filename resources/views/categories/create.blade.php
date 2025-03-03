652d9fb7c8875229d04108300eea6ee36c843190
GeorginaAndrea <geomendezhdz@gmail.com>
2025-03-03 11:45:01 -0600

    Categories: Vista create modificada

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -8,12 +8,8 @@
         <form method="POST">
             @csrf
             <div class="form-group">
-                <label for="title">Nombre</label>
+                <label for="name">Nombre</label>
                 <input type="text" name="title" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="description">Descripción:</label>
-                <textarea name="description" class="form-control" required></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Guardar</button>
         </form>
