67fd8e04484305a1ce3cf5feb66faf5af5e0c73a
GeorginaAndrea <geomendezhdz@gmail.com>
2025-03-07 09:37:15 -0600

    Controller: controler actualizado - store y update

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Collection;
 use App\Http\Controllers\Response;
 use Illuminate\Http\Request;
+use PHPUnit\Runner\DeprecationCollector\Collector;
 
 use function Pest\Laravel\json;
 
@@ -67,4 +68,31 @@ class CollectionController extends Controller
         $collection = Collection::find($collection);
         return view('collections.show', compact('collection'));
     }
+
+   public function edit($collection){
+    $collection = Collection::find($collection);
+    return view('collections.edit',compact('collection'));
+   }
+
+    public function update(Request $request,$collection){
+        try {
+            $validatedData = $request->validate([
+                'name' => 'sometimes|string|max:200'
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->withInput();
+        }
+        $collection = Collection::findOrFail($collection);
+
+        $collection->name = $request->name;
+
+        $collection->save();
+        return redirect("/collections/{$collection->id}")->with('succes','Colleccion actualizada');
+    }
+    public function destroy($collection){
+        $collection = Collection::find($collection);
+        $collection->delete();
+        return redirect('/collections');
+    }
+    
 }
