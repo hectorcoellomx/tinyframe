247bd739bada3dcb3a75969cc77862cb2ce024f3
GeorginaAndrea <geomendezhdz@gmail.com>
2025-03-14 11:29:38 -0600

    Books: vista index actualizada

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -49,13 +49,6 @@
                                 :itemName="$book->title" 
                                 :itemId="$book->id" 
                             />
-                            {{-- <form action="/books/{{$book->id}}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger mb-3">
-                                    <i class="bi bi-trash"></i>
-                                </button>
-                            </form> --}}
                         </td>
                         <td>
                             @if($book->file) <!-- Verifica si el archivo existe -->
