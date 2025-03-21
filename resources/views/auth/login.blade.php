21d6bdc30fae4f8658ac30ecf30221192855a07a
GeorginaAndrea <geomendezhdz@gmail.com>
2025-03-21 11:58:25 -0600

    Verificación agregada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,20 +24,42 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-lg-8 col-md-10">
+           
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">¡Bienvenido de nuevo!</h1>
                                     </div>
 
+                                    <form class="user" method="POST" action="{{ route('auth.authenticate') }}">
+                                        @csrf <!-- Token CSRF para seguridad -->
+                                    
+                                        <!-- Campo de email -->
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="email" name="email" aria-describedby="emailHelp"
+                                                placeholder="Ingresa tu correo electrónico" required>
+                                        </div>
+                                    
+                                        <!-- Campo de contraseña -->
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password" name="password" placeholder="Contraseña" required>
+                                        </div>
+                                    
+                                        <!-- Botón de login -->
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Iniciar sesión
+                                        </button>
+                                    </form>
+
                                     <!-- Formulario de login -->
-                                    <form class="user" method="POST">
+                                    {{-- <form class="user" method="POST">
                                         @csrf <!-- Token CSRF para seguridad -->
 
                                         <!-- Campo de email -->
@@ -52,30 +74,13 @@
                                             <input type="password" class="form-control form-control-user"
                                                 id="password" name="password" placeholder="Contraseña" required>
                                         </div>
-
-                                        <!-- Checkbox "Recordarme" -->
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="remember" name="remember">
-                                                <label class="custom-control-label" for="remember">Recordarme</label>
-                                            </div>
-                                        </div>
-
                                         <!-- Botón de login -->
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Iniciar sesión
                                         </button>
-                                    </form>
+                                    </form> --}}
 
                                     <hr>
-
-                                    <!-- Enlaces adicionales -->
-                                    <div class="text-center">
-                                        <a class="small">¿Olvidaste tu contraseña?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small">¡Crea una cuenta!</a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
