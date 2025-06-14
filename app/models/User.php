dc6844f29b4e94a7a00beecb7792e2dcf803d758
Hector Coello <hector_m3@live.com.mx>
2025-06-14 08:02:20 -0600

    Se quitaron métodos de prueba

--- app/models/User.php
+++ app/models/User.php
@@ -7,32 +7,7 @@ use Core\Databases\DB;
 class User {
 
     public function login($email, $password){
-        return ($email=="admin@gmail.com" && $password == "123");
-    }
-
-    public function single($id){
-        $db = DB::init();
-        return $db->db_select_row("SELECT * FROM users WHERE id=?", [ $id ]);
-    }
-
-    public function list(){
-        $db = DB::init();
-        return $db->db_select("SELECT * FROM users", null);
-    }
-
-    public function insert($name){
-        $db = DB::init();
-        return $db->db_insert("INSERT INTO users (fullname) VALUES (?)", [ $name ]);
-    }
-
-    public function update($id, $name){
-        $db = DB::init();
-        return $db->db_update("UPDATE users SET fullname = ? WHERE id = ?", [ $name, $id ]);
-    }
-
-    public function delete($id){
-        $db = DB::init();
-        return $db->db_delete("DELETE from users WHERE id = ?", [ $id ]);
+        return ($email=="test@example.com" && $password == "123");
     }
 
 }
