c326f3a136f1a9c5ba7e1cb442179455feee3fa9
Hector Coello <hector_m3@live.com.mx>
2025-09-13 06:10:44 -0600

    Corrección en autoloader ahora con estilo PSR-4 con validación de ruta.

--- index.php
+++ index.php
@@ -9,18 +9,23 @@
     $tinyframe_version = "2.0.0";
 
     spl_autoload_register(function ($class) {
-        
+        $baseDir = __DIR__;
         $classPath = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-        $file = __DIR__ . '/' . $classPath . '.php';
+        $parts = explode(DIRECTORY_SEPARATOR, $classPath);
+        $classFile = array_pop($parts);
+        $dirs = array_map('strtolower', $parts);
 
-        if (file_exists($file)) {
-            require_once $file;
+        $file = $baseDir . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $dirs) . DIRECTORY_SEPARATOR . $classFile . '.php';
+
+        $realFile = realpath($file);
+
+        if ($realFile && strpos($realFile, $baseDir) === 0) {
+            require_once $realFile;
         } else {
-            trigger_error("No se encontró el archivo para la clase $class en $file", E_USER_ERROR);
+            trigger_error("No se encontró o no es válido el archivo para la clase $class", E_USER_ERROR);
         }
-        
     });
-    
+
     require './app/Config.php';
     use App\Config;
 
@@ -28,13 +33,7 @@
 
     $tinyframe_bd_driver = ($tinyframe_config->databases_driver!=null) ? strtoupper($tinyframe_config->databases_driver) : $tinyframe_config->databases_driver;
     $tinyframe_status_project = ($tinyframe_config->status_project!=null) ? strtoupper($tinyframe_config->status_project) : $tinyframe_config->status_project;
-
-    if($tinyframe_bd_driver=="ORACLE"){
-        require './core/databases/DB_ORACLE.php';
-    }else{
-        require './core/databases/DB_MYSQL.php';
-    }
-
+    
     if($tinyframe_status_project=="pro"){
         ini_set('display_errors', 0);
         error_reporting(E_ALL ^ E_WARNING);
@@ -42,7 +41,14 @@
         ini_set('display_errors', 1);
         error_reporting(E_ALL);
     }
+
+    require './core/databases/DB_CONFIG.php';
     
+    if($tinyframe_bd_driver=="ORACLE"){
+        require './core/databases/DB_ORACLE.php';
+    }else{
+        require './core/databases/DB_MYSQL.php';
+    }  
     
     $tinyframe_nofound = false;
     $tinyframe_url_response = array();
